8ea951de55b0d6d3f7ab64481ea4a22cf64bce91
Joe <3262202547@qq.com>
2024-10-05 22:00:18 +0800

    p3 task4 add window_func ,pass online test

--- src/execution/window_function_executor.cpp
+++ src/execution/window_function_executor.cpp
@@ -1,14 +1,125 @@
 #include "execution/executors/window_function_executor.h"
+#include <cstdint>
+#include "execution/expressions/column_value_expression.h"
 #include "execution/plans/window_plan.h"
 #include "storage/table/tuple.h"
+#include "type/value_factory.h"
 
 namespace bustub {
 
 WindowFunctionExecutor::WindowFunctionExecutor(ExecutorContext *exec_ctx, const WindowFunctionPlanNode *plan,
                                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {
+  for (const auto &window_function : plan_->window_functions_) {
+    auto window_table = std::make_unique<SimpleWindowHashTable>(window_function.second.type_);
+    auto iter = window_table->Begin();
+    hts_.insert({window_function.first, std::move(window_table)});
+    iters_.insert({window_function.first, iter});
+    if (!window_function.second.order_by_.empty() && !order_by_func_idx_.has_value()) {
+      order_by_func_idx_ = window_function.first;
+    }
+  }
+}
+void WindowFunctionExecutor::Init() {
+  if (!if_init_) {
+    if_init_ = true;
+    child_executor_->Init();
+    Tuple child_tuple;
+    RID child_rid;
+    while (child_executor_->Next(&child_tuple, &child_rid)) {
+      child_tuples_.emplace_back(std::move(child_tuple));
+    }
+    for (auto &ht : hts_) {
+      ht.second->Clear();
+    }
+    if (order_by_func_idx_.has_value()) {
+      SortExecutor::Cmp cmp(plan_->window_functions_.at(order_by_func_idx_.value()).order_by_,
+                            child_executor_->GetOutputSchema());
+      std::sort(child_tuples_.begin(), child_tuples_.end(), cmp);
+    }
+    child_tuples_iter_ = child_tuples_.begin();
+    if (order_by_func_idx_.has_value()) {
+      while (child_tuples_iter_ != child_tuples_.end()) {
+        std::vector<Value> result_value;
+        result_value.clear();
+        result_value.reserve(GetOutputSchema().GetColumnCount());
+        for (uint32_t col_idx = 0; col_idx < GetOutputSchema().GetColumnCount(); col_idx++) {
+          const auto &output_column = plan_->columns_[col_idx];
+          const auto &col_val = dynamic_cast<const ColumnValueExpression &>(*output_column);
+          if (col_val.GetColIdx() == static_cast<uint32_t>(-1)) {
+            const auto &window_function = plan_->window_functions_.at(col_idx);
+            if (window_function.partition_by_.empty()) {
+              AggregateKey agg_key;
+              hts_[col_idx]->InitInsert(agg_key);
+            }
+            auto key = MakeAggregateKey(&*child_tuples_iter_, col_idx);
+            auto val = MakeValue(&*child_tuples_iter_, col_idx);
+            hts_[col_idx]->InsertCombine(key, val);
+            auto v = hts_[col_idx]->GetValue(key);
+            if (v.has_value()) {
+              result_value.emplace_back(std::move(v.value()));
+            }
+          } else {
+            result_value.emplace_back(
+                child_tuples_iter_->GetValue(&child_executor_->GetOutputSchema(), col_val.GetColIdx()));
+          }
+        }
+        Tuple temp_result_tuple(result_value, &GetOutputSchema());
+        result_tuples_.emplace_back(std::move(temp_result_tuple));
+        child_tuples_iter_++;
+      }
+    } else {
+      while (child_tuples_iter_ != child_tuples_.end()) {
+        for (uint32_t col_idx = 0; col_idx < GetOutputSchema().GetColumnCount(); col_idx++) {
+          const auto &output_column = plan_->columns_[col_idx];
+          const auto &col_val = dynamic_cast<const ColumnValueExpression &>(*output_column);
+          if (col_val.GetColIdx() == static_cast<uint32_t>(-1)) {
+            const auto &window_function = plan_->window_functions_.at(col_idx);
+            if (window_function.partition_by_.empty()) {
+              AggregateKey agg_key;
+              hts_[col_idx]->InitInsert(agg_key);
+            }
+            auto key = MakeAggregateKey(&*child_tuples_iter_, col_idx);
+            auto val = MakeValue(&*child_tuples_iter_, col_idx);
+            hts_[col_idx]->InsertCombine(key, val);
+          }
+        }
+        child_tuples_iter_++;
+      }
+      child_tuples_iter_ = child_tuples_.begin();
+      while (child_tuples_iter_ != child_tuples_.end()) {
+        std::vector<Value> result_value;
+        result_value.clear();
+        result_value.reserve(GetOutputSchema().GetColumnCount());
+        for (uint32_t col_idx = 0; col_idx < GetOutputSchema().GetColumnCount(); col_idx++) {
+          const auto &output_column = plan_->columns_[col_idx];
+          const auto &col_val = dynamic_cast<const ColumnValueExpression &>(*output_column);
+          if (col_val.GetColIdx() == static_cast<uint32_t>(-1)) {
+            auto key = MakeAggregateKey(&*child_tuples_iter_, col_idx);
+            auto v = hts_[col_idx]->GetValue(key);
+            if (v.has_value()) {
+              result_value.emplace_back(std::move(v.value()));
+            }
+          } else {
+            result_value.emplace_back(
+                child_tuples_iter_->GetValue(&child_executor_->GetOutputSchema(), col_val.GetColIdx()));
+          }
+        }
+        Tuple temp_result_tuple(result_value, &GetOutputSchema());
+        result_tuples_.emplace_back(std::move(temp_result_tuple));
+        child_tuples_iter_++;
+      }
+    }
+  }
+  result_tuples_iter_ = result_tuples_.begin();
+}
 
-void WindowFunctionExecutor::Init() { throw NotImplementedException("WindowFunctionExecutor is not implemented"); }
-
-auto WindowFunctionExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto WindowFunctionExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  while (result_tuples_iter_ != result_tuples_.end()) {
+    *tuple = Tuple(*result_tuples_iter_);
+    result_tuples_iter_++;
+    return true;
+  }
+  return false;
+}
 }  // namespace bustub
--- src/include/execution/executors/window_function_executor.h
+++ src/include/execution/executors/window_function_executor.h
@@ -12,15 +12,217 @@
 
 #pragma once
 
+#include <bits/iterator_concepts.h>
+#include <cstddef>
+#include <cstdint>
 #include <memory>
+#include <optional>
+#include <unordered_map>
 #include <vector>
 
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
+#include "execution/executors/sort_executor.h"
+#include "execution/plans/aggregation_plan.h"
 #include "execution/plans/window_plan.h"
 #include "storage/table/tuple.h"
-
+#include "type/type.h"
+#include "type/value.h"
+#include "type/value_factory.h"
 namespace bustub {
+
+/**
+ * A simplified hash table that has all the necessary functionality for aggregations.
+ */
+class SimpleWindowHashTable {
+ public:
+  /**
+   * Construct a new SimpleAggregationHashTable instance.
+   * @param agg_exprs the aggregation expressions
+   * @param agg_types the types of aggregations
+   */
+  explicit SimpleWindowHashTable(  // const AbstractExpressionRef window_expr,
+      const WindowFunctionType &window_type)
+      :  // window_expr_{window_expr},
+        window_type_{window_type} {}
+
+  /** @return The initial aggregate value for this aggregation executor */
+  auto GenerateInitialAggregateValue() -> Value {
+    Value value;
+    switch (window_type_) {
+      case WindowFunctionType::CountStarAggregate:
+        // Count start starts at zero.
+        value = ValueFactory::GetIntegerValue(0);
+        break;
+      case WindowFunctionType::CountAggregate:
+      case WindowFunctionType::SumAggregate:
+      case WindowFunctionType::MinAggregate:
+      case WindowFunctionType::MaxAggregate:
+      case WindowFunctionType::Rank:
+        // Others starts at null.
+        value = ValueFactory::GetNullValueByType(TypeId::INTEGER);
+        break;
+    }
+    return value;
+  }
+
+  /**
+   * TODO(Student)
+   *
+   * Combines the input into the aggregation result.
+   * @param[out] result The output aggregate value
+   * @param input The input value
+   */
+  void CombineAggregateValues(Value *result, const Value &input) {
+    switch (window_type_) {
+      case WindowFunctionType::CountStarAggregate: {
+        *result = result->Add(input);
+        break;
+      }
+      case WindowFunctionType::Rank: {
+        if (!input.IsNull()) {
+          if (intend_rank_.GetTypeId() == TypeId::INVALID) {
+            intend_rank_ = ValueFactory::GetIntegerValue(1);
+            *result = intend_rank_;
+            curr_rank_value_ = input;
+          } else {
+            if (curr_rank_value_.CompareEquals(input) == CmpBool::CmpTrue ||
+                curr_rank_value_.CompareEquals(input) == CmpBool::CmpFalse) {
+              Value v1 = ValueFactory::GetIntegerValue(1);
+              intend_rank_ = intend_rank_.Add(v1);
+            }
+            if (curr_rank_value_.CompareEquals(input) == CmpBool::CmpFalse) {
+              *result = intend_rank_;
+              curr_rank_value_ = input;
+            }
+          }
+        }
+        break;
+      }
+      case WindowFunctionType::CountAggregate: {
+        if (!input.IsNull()) {
+          if (result->IsNull()) {
+            *result = ValueFactory::GetIntegerValue(1);
+          } else {
+            Value v1 = ValueFactory::GetIntegerValue(1);
+            *result = result->Add(v1);
+          }
+        }
+        break;
+      }
+      case WindowFunctionType::SumAggregate: {
+        if (!input.IsNull()) {
+          if (result->IsNull()) {
+            *result = input;
+          } else {
+            *result = result->Add(input);
+          }
+        }
+        break;
+      }
+      case WindowFunctionType::MinAggregate: {
+        if (!input.IsNull()) {
+          if (result->IsNull()) {
+            *result = input;
+          } else {
+            *result = result->Min(input);
+          }
+        }
+        break;
+      }
+      case WindowFunctionType::MaxAggregate: {
+        if (!input.IsNull()) {
+          if (result->IsNull()) {
+            *result = input;
+          } else {
+            *result = result->Max(input);
+          }
+        }
+        break;
+      }
+      default:
+        break;
+    }
+    // *result = values;
+  }
+
+  /**
+   * Inserts a value into the hash table and then combines it with the current aggregation.
+   * @param agg_key the key to be inserted
+   * @param agg_val the value to be inserted
+   */
+  void InsertCombine(const AggregateKey &agg_key, const Value &val) {
+    if (ht_.count(agg_key) == 0) {
+      ht_.insert({agg_key, GenerateInitialAggregateValue()});
+    }
+
+    CombineAggregateValues(&ht_[agg_key], val);
+  }
+
+  void InitInsert(const AggregateKey &agg_key) {
+    if (ht_.count(agg_key) == 0) {
+      ht_.insert({agg_key, GenerateInitialAggregateValue()});
+    }
+  }
+
+  auto GetValue(const AggregateKey &agg_key) const -> std::optional<Value> {
+    if (ht_.count(agg_key) > 0) {
+      return ht_.at(agg_key);
+    }
+    return std::nullopt;
+  }
+
+  /**
+   * Clear the hash table
+   */
+  void Clear() { ht_.clear(); }
+
+  /** An iterator over the aggregation hash table */
+  class Iterator {
+   public:
+    /** Creates an iterator for the aggregate map. */
+    explicit Iterator(std::unordered_map<AggregateKey, Value>::const_iterator iter) : iter_{iter} {}
+
+    /** @return The key of the iterator */
+    auto Key() -> const AggregateKey & { return iter_->first; }
+
+    /** @return The value of the iterator */
+    auto Val() -> const Value & { return iter_->second; }
+
+    /** @return The iterator before it is incremented */
+    auto operator++() -> Iterator & {
+      ++iter_;
+      return *this;
+    }
+
+    /** @return `true` if both iterators are identical */
+    auto operator==(const Iterator &other) -> bool { return this->iter_ == other.iter_; }
+
+    /** @return `true` if both iterators are different */
+    auto operator!=(const Iterator &other) -> bool { return this->iter_ != other.iter_; }
+
+   private:
+    /** Aggregates map */
+    std::unordered_map<AggregateKey, Value>::const_iterator iter_;
+  };
+
+  /** @return Iterator to the start of the hash table */
+  auto Begin() -> Iterator { return Iterator{ht_.cbegin()}; }
+
+  /** @return Iterator to the end of the hash table */
+  auto End() -> Iterator { return Iterator{ht_.cend()}; }
+
+ private:
+  /** The hash table is just a map from aggregate keys to aggregate values */
+  std::unordered_map<AggregateKey, Value> ht_{};
+  /** The aggregate expressions that we have */
+  // const AbstractExpressionRef &window_expr_;
+  /** The types of aggregations that we have */
+  const WindowFunctionType &window_type_;
+
+  Value curr_rank_value_;
+  Value intend_rank_;
+};
 
 /**
  * The WindowFunctionExecutor executor executes a window function for columns using window function.
@@ -85,10 +287,39 @@ class WindowFunctionExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); }
 
  private:
+  /** @return The tuple as an AggregateKey */
+  auto MakeAggregateKey(const Tuple *tuple, uint32_t window_func_idx) -> AggregateKey {
+    std::vector<Value> keys;
+    for (const auto &expr : plan_->window_functions_.at(window_func_idx).partition_by_) {
+      keys.emplace_back(expr->Evaluate(tuple, child_executor_->GetOutputSchema()));
+    }
+    return {keys};
+  }
+  auto MakeValue(const Tuple *tuple, uint32_t window_func_idx) -> Value {
+    if (plan_->window_functions_.at(window_func_idx).type_ == WindowFunctionType::Rank) {
+      return plan_->window_functions_.at(window_func_idx)
+          .order_by_[0]
+          .second->Evaluate(tuple, child_executor_->GetOutputSchema());
+    }
+    return plan_->window_functions_.at(window_func_idx).function_->Evaluate(tuple, child_executor_->GetOutputSchema());
+  }
+
+ private:
   /** The window aggregation plan node to be executed */
   const WindowFunctionPlanNode *plan_;
 
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+
+  std::unordered_map<uint32_t, std::unique_ptr<SimpleWindowHashTable>> hts_;
+  std::unordered_map<uint32_t, SimpleWindowHashTable::Iterator> iters_;
+  std::optional<uint32_t> order_by_func_idx_ = std::nullopt;
+
+  std::vector<Tuple> child_tuples_;
+  std::vector<Tuple>::iterator child_tuples_iter_ = child_tuples_.begin();
+
+  std::vector<Tuple> result_tuples_;
+  std::vector<Tuple>::iterator result_tuples_iter_ = result_tuples_.begin();
+  bool if_init_ = false;
 };
 }  // namespace bustub
